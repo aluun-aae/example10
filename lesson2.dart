2407fdaf13177ccac5ca8faf7ef2de52dc754f03
aluun <a.akjigitov@averspay.kg>
2022-10-07 19:50:15 +0600

    first commit

---
+++ lesson2.dart
@@ -0,0 +1,42 @@
+void main() {
+  String name = "Aluun";
+  int mark;
+  mark = 40;
+  double pi = 4;
+  int a = 10;
+  int b = 20;
+  final c;
+  c = "312";
+  var q = "a";
+  const l = 1321;
+  q = "fdas";
+  String msg = "добро пожаловать во Flutter";
+  String msg1 = 'добро пожаловать во Flutter';
+  List array = [
+    "Adam"
+        "Sanjar",
+    true,
+    "Aluun",
+    12,
+  ];
+  Map student = {'StudentName': "Aluun", 'age': 23, 'name3': "♥"};
+  print(student);
+  var heart_symbold = '\u2665';
+
+  dynamic das = 20;
+  print(das);
+  print(das);
+
+  print(heart_symbold);
+  bool isNumber = true;
+  bool isString = false;
+  print(array);
+  print(isNumber);
+  print(isString);
+  print(msg);
+  print(msg1);
+  print(a + b); // 10 + 20
+  print(name);
+  print(mark);
+  print(pi);
+}
